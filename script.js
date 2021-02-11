2d235a18ba17689402029f52a07040d0d0db4470
BeckyLashua <beckylashua@gmail.com>
2021-02-11 16:48:11 -0500

    Starting to add asynchronous load function

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ form1_submit.addEventListener('click', (event) => {
     let city = document.getElementById('city').value;
     let country = document.getElementById('country').value;
     let req = new XMLHttpRequest();
-    req.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + country + "&appid=" + appid, false);
+    req.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=" 
+                    + city + "," + country + "&appid=" + appid, true);
+    req.addEventListener('load', () => {
+        if(req.status >= 200 && req.status < 400) {
+            let response1 = JSON.parse(req.responseText);
+            document.getElementById('weather1').textContent = response1.weather[0].description;
+            document.getElementById('temp1').textContent = response1.main.temp;
+            document.getElementById('wind1').textContent = response1.wind.speed;
+        } else {
+            console.log("Error in network request: " + req.statusText);
+        }
+    });
     req.send(null);
-    let response1 = JSON.parse(req.responseText);
-    document.getElementById('weather1').textContent = response1.weather[0].description;
-    document.getElementById('temp1').textContent = response1.main.temp;
-    document.getElementById('wind1').textContent = response1.wind.speed;
     event.preventDefault();
 });
 
@@ -19,7 +26,18 @@ form2_submit.addEventListener('click', (event) => {
     let zipcode = document.getElementById('zipcode').value;
     let country2 = document.getElementById('country2').value;
     let req2 = new XMLHttpRequest();
-    req2.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip="+ zipcode + "," + country2 + "&appid=" + appid, false); 
+    req2.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip="
+                    + zipcode + "," + country2 + "&appid=" + appid, true); 
+    req2.addEventListener('load', () => {
+        if(req2.status >= 200 && req2.status < 400) {
+            let response2 = JSON.parse(req2.responseText);
+            document.getElementById('weather2').textContent = response2.weather[0].description;
+            document.getElementById('temp2').textContent = response2.main.temp;
+            document.getElementById('wind2').textContent = response2.wind.speed;
+        } else {
+            console.log("Error in network request: " + req2.statusText);
+        }
+    });
     req2.send(null);
     let response2 = JSON.parse(req2.responseText); 
     document.getElementById('weather2').textContent = response2.weather[0].description;
